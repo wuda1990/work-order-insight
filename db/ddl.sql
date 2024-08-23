d27f1c06dcb70ce3788d8403117bc753e4511640
huajun.wu <huajun.wu@atrenew.com>
2024-08-23 10:52:19 +0800

    add scripts

--- db/ddl.sql
+++ db/ddl.sql
@@ -3,7 +3,7 @@ CREATE TABLE `t_statistics_operator`
     `id`        bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
     `operator`  varchar(32)         NOT NULL COMMENT '',
     `status`    int(10) unsigned    NOT NULL COMMENT 'task status',
-    `count`     bigint(20) unsigned NOT NULL COMMENT 'task count',
+    `count`     bigint(20)          NOT NULL COMMENT 'task count',
     `create_dt` datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
     `update_dt` datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '',
     PRIMARY KEY (`id`),
@@ -13,5 +13,16 @@ CREATE TABLE `t_statistics_operator`
   DEFAULT CHARSET = utf8mb4 COMMENT ='operator statistics table'
 ;
 
-select *
-from t_statistics_operator;
+CREATE TABLE `t_statistics_department`
+(
+    `id`         bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+    `department` int(10)             NOT NULL COMMENT '',
+    `status`     int(10) unsigned    NOT NULL COMMENT 'task status',
+    `count`      bigint(20)          NOT NULL COMMENT 'task count',
+    `create_dt`  datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
+    `update_dt`  datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '',
+    PRIMARY KEY (`id`),
+    KEY `idx_update_dt` (`update_dt`),
+    UNIQUE KEY `uk_operator_status` (`department`, `status`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='department statistics table'
